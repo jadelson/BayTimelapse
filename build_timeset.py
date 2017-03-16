73c6d0956c8d89b83d86e98a4d67dcb356426432
jadelson <joe.adelson@gmail.com>
2017-03-16 14:45:09 -0700

    Some debugging, some file removal, landsat7 capability work

--- build_timeset.py
+++ build_timeset.py
@@ -17,15 +17,18 @@ import build_dataset
 
 # Set directory locations
 base_dir = '/Volumes/Stella/'
-
-raw_sat_directory = base_dir + 'landsat_order/landsat8_convert/'
+working_dir = './'
+raw_sat_directory = base_dir + 'landsat_order/landsat7_convert/'
 sat_filter_directory = base_dir + 'sat_on_mesh/'
+sat = 'l7'
 
-sat = 'l8'
+
 if sat == 'l8':
-    l8_keys = [ 'LAT', 'LON',  'RHOW_443', 'RHOW_483', 'RHOW_561', 'RHOW_655', 'RHOW_865'] #Landsat 8
-    l8_test_key = 'RHOW_655'
-
+    sat_keys = [ 'LAT', 'LON',  'RHOW_443', 'RHOW_483', 'RHOW_561', 'RHOW_655', 'RHOW_865'] #Landsat 8
+    sat_test_key = 'RHOW_655'
+if sat == 'l7':
+    sat_keys = [ 'LAT', 'LON',  'RHOW_479', 'RHOW_561', 'RHOW_661', 'RHOW_835'] #Landsat 7
+    sat_test_key = 'RHOW_661'
 
 
 # A function that writes a combined satellite and tidal dataset
@@ -39,15 +42,15 @@ def write_sat_data(satnc, tide_data, date):
     """
     sat_data = {}
     final_data = {}
-    for k in l8_keys:
+    for k in sat_keys:
         final_data[k] = []
     for k in tide_data.keys():
         final_data[k] = []
         
-    for s in l8_keys:
+    for s in sat_keys:
         sat_data[s] = np.asarray(satnc[s][:,:])
 
-    if np.min(sat_data['RHOW_655']>= 9.96921e35):
+    if np.min(sat_data[sat_test_key]>= 9.96921e35):
         return None
     
     maxlat = np.max(sat_data['LAT'])
@@ -64,14 +67,17 @@ def write_sat_data(satnc, tide_data, date):
     for i in range(0,m[0]):
 
         if lat[i] > maxlat or lat[i] < minlat or lon[i] > maxlon or lon[i] < minlon:
+#            print('fail1')
             continue
         x,y, A = get_coord(sat_data,lat[i],lon[i],n[0],n[1],A)
         if x >= n[0] or y >= n[1]:
+#            print('fail2')
             continue
-        if sat_data['RHOW_655'][x,y] >= 9.96921e35 or np.isnan(sat_data[l8_test_key][x,y]):
+        if sat_data[sat_test_key][x,y] >= 9.96921e35 or np.isnan(sat_data[sat_test_key][x,y]):
+#            print('fail3')
             continue
         
-        linedata = {k:sat_data[k][x,y] for k in l8_keys}
+        linedata = {k:sat_data[k][x,y] for k in sat_keys}
         for k in tide_data.keys():
                 linedata[k] = tide_data[k][i]
     
@@ -91,23 +97,29 @@ def write_sat_data(satnc, tide_data, date):
 
 
 def sat_worker(filename, tide_data):
+    """
+    Extract satelite data onto the tidal model grid
+    
+    :param filename: name of the Acolite corrected netcdf satellite file
+    :param tide_data: dict of tidal data at each point in domain
+    """
     model = nc.Dataset(raw_sat_directory+filename)
     if not tide_data == None:
-        sat_tide_data = write_sat_data(model, tide_data, model.DATE+'_'+model.HOUR+model.MINUTE+model.SECOND[0:2])
-        if not sat_tide_data == None:
+        combined_sat_data = write_sat_data(model, tide_data, model.DATE+'_'+model.HOUR+model.MINUTE+model.SECOND[0:2])
+        if not combined_sat_data == None:
             print('Sat: ' + raw_sat_directory+filename)
-            print(len(tide_data['lat']), len(sat_tide_data['lat']))
+            print(len(tide_data['lat']), len(combined_sat_data['lat']))
     model.close()
-    return    
+    return combined_sat_data   
     
 if __name__ == "__main__":
     # Load tide model data (requires current velocities, water level, and depth)
     blank_date = datetime.strptime('20160301 18 30 00','%Y%m%d %H %M %S')        
     tide_data = build_dataset.find_tide_data_by_time(blank_date)
     sat_inputs = [k for k in os.listdir(raw_sat_directory) if k.endswith('.nc')]
-    sat_worker(sat_inputs[1], tide_data)
-#    results = Parallel(n_jobs=3)(delayed(sat_worker)(filename) for filename in sat_inputs)
-#   
+#    blah = sat_worker(sat_inputs[3], tide_data)
+    results = Parallel(n_jobs=4)(delayed(sat_worker)(filename, tide_data) for filename in sat_inputs)
+
 
 ##                     
     full_data = {}
@@ -123,9 +135,9 @@ if __name__ == "__main__":
                 if not k in full_data.keys():
                     full_data[k] = np.array([])
                 full_data[k] = np.append(full_data[k], combined_sat_data[k])
-            full_data['date'] = np.append(full_data['date'],[datetime.strptime(filename[29:44],'%Y%m%d_%H%M%S')]*combined_sat_data[k])
+            full_data['date'] = np.append(full_data['date'],[datetime.strptime(filename[29:44],'%Y%m%d_%H%M%S')]*len(combined_sat_data[k]))
             f.close()
 ##            
-#    with open('/Users/jadelson/Dropbox/phdResearch/AllOptical/timelapse_work/full_dataset.dk','wb') as f:
-#        pickle.dump(full_data, f)
+    with open(working_dir+sat+'_full_dataset.dk','wb') as f:
+        pickle.dump(full_data, f)
 ##        
